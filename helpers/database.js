0aa3648f1da99f76488a80ac8144975de7cc51b7
Ignacio909 <imedinillad@gmail.com>
2025-09-17 11:44:03 -0400

    Primer Push, configuración inicial con instalaciones

---
+++ helpers/database.js
@@ -0,0 +1,24 @@
+const Sequelize= require("sequelize");
+const dotenv = require("dotenv").config();
+
+const databaseName = process.env.DB_NAME;
+const password = process.env.DB_PASSWORD;
+const user = process.env.DB_USER;
+const dialect = process.env.DB_DIALECT;
+const host = process.env.HOST;
+
+//Conexion con la BD
+const sequelize = new Sequelize(databaseName, user, password, {
+    host: host,
+    dialect: dialect,
+    loggin: false,
+});
+
+//Funcion para comprobar que salio bien
+sequelize
+    .autenticate()
+    .then(()=>{
+        console.log("Conexion establecida correctamente")})
+    .catch((err)=>{
+        console.log("Error al conectarse con la Base de Datos ")})
+module.exports = sequelize;    
---
+++ models/personas.js
@@ -0,0 +1,6 @@
+const { DataTypes } = require ("sequelize");
+const sequelize = require("../helpers/database");
+
+const Personas = sequelize.define("personas",{
+    user_name 
+})
